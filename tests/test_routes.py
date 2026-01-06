efc3a6014d340c1418d0a9b629a5b1409b805d9d
abi-seg <abirami1488@gmail.com>
2026-01-06 23:06:52 +0100

    Test: Verify points are deducted after a valid booking

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from server import app
+from server import app,clubs
 
 import pytest
 
@@ -13,6 +13,30 @@ def client():
         yield client
 
 def test_invalid_login_email(client):
-    """Test login with wrong email does not crash."""
+
+    """
+    Test login with wrong email does not crash.
+    
+    """
     response = client.post('/showSummary', data={'email': 'wrong@email.com'})
     assert response.status_code in (200, 302)
+
+def test_points_deducted_after_booking(client):
+    
+    """
+     Ensure that club points are lowered after a successful booking.
+
+     """
+    # first: get current points
+    start_points = int([c for c in clubs if c["name"] == "Simply Lift"][0]["points"])
+
+    #Book 2 places
+    client.post('/purchasePlaces', data = {
+        'competition': 'Fall Classic',
+        'club': 'Simply Lift',
+        'places': '2'
+    })
+
+    # New points should be start - 2
+    end_points = int([c for c in clubs if c["name"] == "Simply Lift"][0]["points"])
+    assert end_points == start_points - 2
